78c02f1a21d0c9d7708acb191190659ce0cc8bf5
202797 <36769821+202797@users.noreply.github.com>
2018-05-07 14:24:06 -0400

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,47 @@
+package com.anderson.bryce.fidgetapp;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends ListActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.main);
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, ACTIVITIES));
+        getListView().setTextFilterEnabled(true);
+    }
+
+    static final String[] ACTIVITIES = new String[]{"Vibration", "Color Organization", "Audio", "Dot Shuffling", "Dials & Sliders"};
+
+    protected void onListItemClick(ListView l, View v, int position, long id)
+    {
+        super.onListItemClick(l, v, position, id);
+        //Object o = this.getListAdapter().getItem(position);
+       // String activity = o.toString();
+       // Toast.makeText(this, "You have chosen the activity: " + " " + activity, Toast.LENGTH_LONG).show();
+        Intent intent = null;
+
+        if (position == 0)
+        {
+            intent = new Intent(this, Vibrations.class);
+        }
+        else if (position == 1)
+        {
+           // intent = new Intent(this, others.class);
+        }
+
+        if (intent != null)
+        {
+            startActivity(intent);
+        }
+    }
+}
---
+++ Vibrations.java
@@ -0,0 +1,13 @@
+package com.anderson.bryce.fidgetapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Vibrations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vibrations);
+    }
+}
