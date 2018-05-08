6254aef5ff688b714a9067d4f7bd3777495820c0
202797 <36769821+202797@users.noreply.github.com>
2018-05-07 21:19:48 -0400

    Add files via upload

---
+++ ColorOrganization.java
@@ -0,0 +1,13 @@
+package com.anderson.bryce.fidgetapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ColorOrganization extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_color_organization);
+    }
+}
