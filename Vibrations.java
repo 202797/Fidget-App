ae8d1fcfbb48f37055265c4c8731ca714ede9437
202797 <36769821+202797@users.noreply.github.com>
2018-05-11 17:16:08 -0400

    Update Vibrations.java

--- Vibrations.java
+++ Vibrations.java
@@ -1,40 +1,40 @@
 package com.anderson.bryce.fidgetapp;
 
-import android.app.Activity;
-import android.content.Context;
+//import android.content.Context;
 import android.os.Bundle;
 import android.os.Vibrator;
-import android.view.View;
-import android.widget.Button;
-import android.view.View.OnClickListener;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 
-public class Vibrations extends Activity
+
+public class Vibrations extends AppCompatActivity
 {
-    private Button button1;
+ //   Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE); //this is what is crashing the app
+    private int vibe = 300000;
+    long l = vibe;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vibrations);
-        button1 = (Button)findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener() 
-        {
-
-            Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-
-            public void onClick(View v)
-            {
-                if (v == button1)
-                {
-                    vibrator.vibrate(300000);
-                }
-                else
-                {
-                    vibrator.cancel();
-                }
-            }
-        });
-
     }
+
+    public boolean onTouchEvent(MotionEvent event)
+    {
+
+
+        if (event.getAction() == MotionEvent.ACTION_UP)
+        {
+            ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(300000);
+        }
+        else
+        {
+            ((Vibrator) getSystemService(VIBRATOR_SERVICE)).cancel();
+        }
+
+        return true;
+
+    }// end of onTouchEvent
+
 }
