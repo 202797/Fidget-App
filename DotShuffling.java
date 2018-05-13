10bb80fbc7085e939d4cdee80f7b22746a6194df
202797 <36769821+202797@users.noreply.github.com>
2018-05-13 01:26:13 -0400

    Update DotShuffling.java

--- DotShuffling.java
+++ DotShuffling.java
@@ -2,12 +2,48 @@ package com.anderson.bryce.fidgetapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.widget.TextView;
 
-public class DotShuffling extends AppCompatActivity {
+public class DotShuffling extends AppCompatActivity implements SensorEventListener
+{
+    private Sensor mySensor;
+    private SensorManager SM;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dot_shuffling);
+
+        SM = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mySensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        SM.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        TextView dot1 = (TextView) findViewById(R.id.dot1);
+        TextView dot2 = (TextView) findViewById(R.id.dot2);
+        TextView dot3 = (TextView) findViewById(R.id.dot3);
+        TextView dot4 = (TextView) findViewById(R.id.dot4);
+        TextView dot5 = (TextView) findViewById(R.id.dot5);
+        TextView dot6 = (TextView) findViewById(R.id.dot6);
+        TextView dot7 = (TextView) findViewById(R.id.dot7);
+        TextView dot8 = (TextView) findViewById(R.id.dot8);
+        TextView dot9 = (TextView) findViewById(R.id.dot9);
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy)
+    {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event)
+    {
+        event.values[0];
     }
 }
